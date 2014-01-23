2c50f8378f50947570081c1d7d40d123d2b5904c
Omar Diab <o.s.diab@gmail.com>
2014-01-23 13:26:56 -0800

    fix(footer, resize): fix resize order and sticky footer positioning

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,31 @@
 $(window).resize(function() {
-  $('#wrapper').css({
-    marginBottom: -$('#credit').outerHeight()
-  });
-
-  $('#logo_stripe').css({
-    top: $('#logo_pane').position().top,
-    height: $('#logo_pane').outerHeight()
+  // affects document height, in document flow: resize first.
+  $('#left').css({
+    width: $('#name').height()
   });
 
   $('#me').css({
     height: $('#left').height() + parseInt($('#left').css('padding-top'))
   });
 
-  $('#left').css({
-    width: $('#name').height()
-  });
-
-  $('#shade').css({
-    height: $(document).height()
-  });
-
+  // not in document flow, dependent on positions; do last
   $('#credit_bg').css({
     height: $('#credit_contents').height(),
     width: $('#credit_contents').width(),
     left: $('#credit_contents').offset().left,
     top: $('#credit_contents').offset().top
   });
+
+  $('#shade').css({
+    height: $('body').height()
+  });
+
+  $('#logo_stripe').css({
+    top: $('#logo_pane').position().top,
+    height: $('#logo_pane').outerHeight()
+  });
 });
 
 $(window).load(function() {
   $(window).resize();
-  $(window).resize(); // TODO: hacky, find out why this occurs
 });
