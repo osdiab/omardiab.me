d31150b73d9e121514f4eb3d0a3e20d22c248774
Omar Diab <o.s.diab@gmail.com>
2014-01-22 22:05:44 -0800

    feat(heading): add heading and image

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,24 @@
 $(window).resize(function() {
+  // center page content vertically and horizontally
   $('#wrapper').css({
     top: ($(window).height() - $('#content').outerHeight()) / 2
+  });
+
+  $('#logo_stripe').css({
+    top: $('#logo_pane').position().top,
+    height: $('#logo_pane').outerHeight()
+  });
+
+  $('#me').css({
+    height: $('#left').height()
+  });
+
+  $('#left').css({
+    width: $('#name').height()
   });
 });
 
 $(window).load(function() {
   $(window).resize();
+  $(window).resize(); // TODO: hacky, find out why this occurs
 });
